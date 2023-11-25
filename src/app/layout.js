d13418cc5949345c685e972499161bd190f7f653
calmbing <mfathulqorib97@gmail.com>
2023-11-25 22:21:27 +0700

    update metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,8 @@ export const metadata = {
   title: "Noteme",
   description: "Simple note app",
   name: "viewport",
-  content: "width=device-width, initial-scale=1.0",
+  content:
+    "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
 };
 
 export default async function RootLayout({ children }) {
