797ee4e06cb39c7e1ebca0d60c9b0dd111b0d3a9
calmbing <mfathulqorib97@gmail.com>
2023-11-26 01:44:36 +0700

    fix layout bug

--- src/component/noteeditor/NoteFooter.jsx
+++ src/component/noteeditor/NoteFooter.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const NoteFooter = () => {
   return (
-    <footer className="py-2 text-white pr-4 pl-4 flex gap-2 items-center justify-between bg-[#264653] ">
+    <footer className="py-[0.55rem] text-white pr-4 pl-4 flex gap-2 items-center justify-between bg-[#264653] z-10">
       <div>Noteme.</div>
       <div className="text-xs align-middle text-center">
         With support from Devscale Indonesia ❤️
